ec6e44cc32ba4bffa9b8e3c136cb6f1f4c971958
Srishti Sharma <imsrishtisharma@gmail.com>
2024-12-27 23:49:42 +0530

    Adding data to JSON file

--- src/main/java/ms/hospital/getindianbankbranches/ApiPackage/BranchRS.java
+++ src/main/java/ms/hospital/getindianbankbranches/ApiPackage/BranchRS.java
@@ -1,18 +1,31 @@
 package ms.hospital.getindianbankbranches.ApiPackage;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.IOException;
 
 @RestController
 @RequestMapping("api/IB")
 public class BranchRS {
 
-    @RequestMapping(value = "/fetchDetails", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String fetchDetails() throws IOException {
-        return new ProviderImpl().dataProvider().toString();
+    @RequestMapping(value = "/populateData", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ObjectNode fetchDetails(@RequestBody JSONObject requestBody) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode root = mapper.createObjectNode();
+        root = mapper.readValue(new ProviderImpl().dataProvider().toString(), ObjectNode.class);
+        mapper.writeValue(new File("src\\main\\resources\\static\\IBAllBranchDetails.json"), root);
+
+        root.removeAll();
+        root.put("status", "success");
+        root.put("message", "JSON file created successfully");
+        return root;
     }
 }
